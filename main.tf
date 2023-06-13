ada20d33505f4e593ad95c515a872523eed57967
nollenr <nollenr@gmail.com>
2023-06-13 13:07:52 -0700

    multi cloud settings not working as intended.  undoing changes.

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ resource "tls_locally_signed_cert" "user_cert" {
 
 module "crdb-region-0" {
   # use the https clone url from github, but without the "https://"
-  source = "github.com/nollenr/AWS-Terraform-CRDB-Module.git?ref=multi-cloud"
+  source = "github.com/nollenr/AWS-Terraform-CRDB-Module.git"
 
   providers = {
     aws = aws.region-0
