20ea9a32c82d20f00998e1e2955ac7e216815e2a
nollenr <nollenr@gmail.com>
2025-09-30 12:42:54 -0700

    changes to outputs for accessing ssh IPs

--- outputs.tf
+++ outputs.tf
@@ -15,48 +15,30 @@
 
 output "module-crdb-region-0-app_node_public_ip" {
   description = "The public IP address of the 1st Region app node, if created."
-  value = module.crdb-region-0.var.include_app == "yes" && module.crdb-region-0.var.create_ec2_instances == "yes" ? (
-    # If count is 1, access the first (and only) instance in the list
-    module.crdb-region-0.aws_instance.app[0].public_ip
-  ) : (
-    # If count is 0, return null (or an empty string)
-    null
-  )
+  value = module.crdb-region-0.app_node_public_ip
 }
 
 output "module-crdb-region-1-app_node_public_ip" {
-  description = "The public IP address of the 1st Region app node, if created."
-  value = module.crdb-region-1.var.include_app == "yes" && module.crdb-region-1.var.create_ec2_instances == "yes" ? (
-    # If count is 1, access the first (and only) instance in the list
-    module.crdb-region-0.aws_instance.app[0].public_ip
-  ) : (
-    # If count is 0, return null (or an empty string)
-    null
-  )
+  description = "The public IP address of the 2nd Region app node, if created."
+  value = module.crdb-region-1.app_node_public_ip
 }
 
 output "module-crdb-region-2-app_node_public_ip" {
-  description = "The public IP address of the 1st Region app node, if created."
-  value = module.crdb-region-2.var.include_app == "yes" && module.crdb-region-2.var.create_ec2_instances == "yes" ? (
-    # If count is 1, access the first (and only) instance in the list
-    module.crdb-region-0.aws_instance.app[0].public_ip
-  ) : (
-    # If count is 0, return null (or an empty string)
-    null
-  )
+  description = "The public IP address of the 3rd Region app node, if created."
+  value = module.crdb-region-2.app_node_public_ip
 }
 
 output "module-crdb-region-0-public_ips_by_az" {
-  description = "CockroachDB Node public IPs assigned to interfaces by AZ."
-  value       = module.crdb-region-0.local.crdb_public_ips_by_az
+  description = "CockroachDB Node public IPs assigned to interfaces by AZ in 1st Region."
+  value       = module.crdb-region-0.public_ips_by_az
 }
 
 output "module-crdb-region-1-public_ips_by_az" {
-  description = "CockroachDB Node public IPs assigned to interfaces by AZ."
-  value       = module.crdb-region-1.local.crdb_public_ips_by_az
+  description = "CockroachDB Node public IPs assigned to interfaces by AZ in 2nd Region."
+  value       = module.crdb-region-1.public_ips_by_az
 }
 
 output "module-crdb-region-2-public_ips_by_az" {
-  description = "CockroachDB Node public IPs assigned to interfaces by AZ."
-  value       = module.crdb-region-2.local.crdb_public_ips_by_az
+  description = "CockroachDB Node public IPs assigned to interfaces by AZ in 3rd Region."
+  value       = module.crdb-region-2.public_ips_by_az
 }
